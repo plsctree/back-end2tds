f99175ebeaec8159fd6673bbee4863d9d7481dac
mikael <mariane.barth.pinheiro@escola.pr.gov.br>
2026-03-10 11:39:31 -0300

    Add files via upload

---
+++ endereco.js
@@ -0,0 +1,8 @@
+console.log("Endereço Completo");
+let rua = "Rua das Flores";
+let numero = 127;
+let cidade = "Curitiba";
+
+let endereco = rua + ", " + numero + " - " + cidade;
+//console.log(endereco)
+console.log("O endereço correto é: " + endereco);
