60b4d1c78866296c812b7a21f76d9a0b8b55b005
mikael <mariane.barth.pinheiro@escola.pr.gov.br>
2026-03-24 14:45:19 +0000

    commit

---
+++ interpolacao.js
@@ -0,0 +1,10 @@
+const nome = "Ana";
+const idade = "21";
+const AnoAtual = 2026;
+
+const AnoNascimento = AnoAtual - idade;
+const IdadeEmDezAnos = idade + 10;
+
+console.log("O ano de nascimento é: " + AnoNascimento);
+console.log(`${nome} possui ${idade} anos. ` );
+console.log(`Daqui a 10 anos, terá ${IdadeEmDezAnos} anos.`);
