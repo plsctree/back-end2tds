7223890fd147608ef27367ca2f1b7294069effa2
mikael <mariane.barth.pinheiro@escola.pr.gov.br>
2026-04-15 13:43:51 +0000

    lista.js

---
+++ lista.js
@@ -0,0 +1,10 @@
+const alunos = ["Jaehyun", "Jungwoo", "Doyoung"];
+
+alunos.push("Dojaejung");
+alunos.unshift("Neo");
+console.log(alunos);
+
+console.log(alunos.indexOf("Dojaejung")); //Retorna o índice do array
+console.log(alunos.indexOf("Mark"));
+
+console.log(alunos.includes("Mark)"));
