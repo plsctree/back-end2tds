07376cc1ff1ec30c0aed943f3f319e041351dfbc
mikael <mariane.barth.pinheiro@escola.pr.gov.br>
2026-04-29 13:52:13 +0000

    versao2

--- sgp.js
+++ sgp.js
@@ -6,16 +6,28 @@ const somarRodada = function(acumulador, pontosDaFase) {
     return novaSoma;
 };
 
-const pontuacaoFinal = historicoDePontos.reduce(somarRodada, 0);
-return pontuacaoFinal;
+    const pontuacaoFinal = historicoDePontos.reduce(somarRodada, 0);
+    return pontuacaoFinal;
 };
 
 function filtrarMissoesConcluidas(listaDeMissoes) {
     const verificarStatusDaMissao = function(missao) {
-        const foiFinalizada = missao.status === "concluida"; // === igualdade RESTRITA
+        const foiFinalizada = missao.status === "concluida"; // === igualdade ESTRITA
         return foiFinalizada;
     };
 
     const apenasConcluidas = listaDeMissoes.filter(verificarStatusDaMissao);
     return apenasConcluidas;
 };
+
+function atualizarInventario(inventarioAtual, acao, nomeDoItem) {
+    let novoInventario;
+
+    if(acao === "pegar") {
+        const inventarioComItemNovo = [inventarioAtual, nomeDoItem];
+        novoInventario = inventarioComItemNovo;
+    } else if (acao === "desartar") 
+}
+
+const pontosDoJogador = [100, 50, 200, 10]
+console.log(pontosDoJogador);
