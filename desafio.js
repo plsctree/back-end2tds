eac3572ea60e8e6c4de63c2516c9f2b47395a950
mikael <mariane.barth.pinheiro@escola.pr.gov.br>
2026-05-20 13:52:30 +0000

    v3

--- desafio.js
+++ desafio.js
@@ -1,7 +1,9 @@
 // 🔴 1. Declarar 5 variáveis para representar o cadastro de um aluno
 // 🔴 2. Crie duas variáveis númericas e exiba uma soma
 // 🔴 3. Calcular a média final. Declarar nota de prova e nota de trabalho. Calcular também a difrença da maior nota e menor
-// Fazer uma saída de forma interpolada (´${}´) 
+// 🔴 4. 0 aluno é aprovado se a média for maior ou igual a 6 e a frequência for maior que 75. Crie a variáveis neceária para realizar esta comparação e exibir o 
+// resultado
+// Fazer uma saída de forma interpolada (´${}´) ] 
 
 const nomeCompleto = "Luana Coutto";
 const idade = 20;
@@ -34,3 +36,9 @@ console.log(`Média Final: ${mediaFinal.toFixed(1)}`);
 console.log(`Maior Nota: ${maiorNota}`);
 console.log(`Menor Nota: ${menorNota}`);
 console.log(`Diferença entre as notas: ${diferenca.toFixed(1)}`);
+
+let frequencia = 80; 
+let aprovado = mediaFinal >= 6 && frequencia > 75;
+
+console.log(`Frequência: ${frequencia}%`);
+console.log(`Status de Aprovação: ${aprovado ? "Aprovado(a)" : "Reprovado(a)"}`);
